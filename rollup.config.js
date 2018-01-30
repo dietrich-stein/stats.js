3db36b0f4b86e2f2907cc0a16730068b556cc80f
zprodev <zproject.develop@gmail.com>
2018-01-30 11:49:20 +0900

    Support development of es6

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/Stats.js',
   output: {
     file: 'build/stats.js',
     format: 'umd',
-    name: 'Stats'
-  }
+    name: 'Stats',
+  },
+  plugins: [
+     babel(),
+  ],
 };
